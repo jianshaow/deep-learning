4261ad83a782b2bcd84ad3b5d39b073a75f40108
Jianshao <jms_wu@hotmail.com>
2019-08-20 18:01:13 +0800

    add tranning visualization

--- xor/xor-learning.py
+++ xor/xor-learning.py
@@ -5,8 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 SEQUENCE_SIZE = 10
-TRAINNING_DATA_SIZE = 10000
+TRAINNING_DATA_SIZE = 5000
 TEST_DATA_SIZE = 100
+TRAINNING_EPOCH = 20
 
 
 def seq_xor(sequence_pair):
@@ -63,41 +64,61 @@ def create_model():
     return model
 
 
-def show_result(data, result):
+def show_history(history):
     plt.figure(figsize=(9, 6))
-    plt.grid(False)
 
-    plt.subplot(3, 1, 1)
-    plt.xticks(range(10))
-    plt.yticks(range(21))
-    plt.ylabel("seq 1")
-    plt.bar(range(10), data[0])
+    accuracy = history.history['acc']
+    loss = history.history['loss']
 
-    plt.subplot(3, 1, 2)
-    plt.xticks(range(10))
-    plt.yticks(range(2))
-    plt.ylabel("seq 2")
-    plt.bar(range(10), data[1])
-
-    plt.subplot(3, 1, 3)
-    plt.xticks(range(10))
-    plt.yticks(range(2))
-    plt.ylabel("xor")
-    plt.bar(range(10), result)
+    plt.plot(accuracy, c='blue')
+    for i in range(len(accuracy)):
+        plt.scatter(i, accuracy[i], color="blue")
+    plt.plot(loss, color='red')
+    for i in range(len(loss)):
+        plt.scatter(i, loss[i], color="red")
+    plt.title('trainning history')
+    plt.xlabel('epoch')
+    plt.xticks(range(TRAINNING_EPOCH), range(1, TRAINNING_EPOCH + 1))
+    plt.legend(['accuracy', 'loss'], loc='center right')
 
     plt.show()
 
 
-if __name__ == "__main__":
+def show_result(data, result):
+    plt.figure(figsize=(9, 6))
+
+    plt.subplot(3, 1, 1)
+    plt.xticks(range(SEQUENCE_SIZE), range(1, SEQUENCE_SIZE + 1))
+    plt.ylabel('seq 1')
+    plt.bar(range(SEQUENCE_SIZE), data[0])
+
+    plt.subplot(3, 1, 2)
+    plt.xticks(range(SEQUENCE_SIZE), range(1, SEQUENCE_SIZE + 1))
+    plt.ylabel('seq 2')
+    plt.bar(range(SEQUENCE_SIZE), data[1])
+
+    plt.subplot(3, 1, 3)
+    plt.xticks(range(SEQUENCE_SIZE), range(1, SEQUENCE_SIZE + 1))
+    plt.ylabel('xor')
+    plt.bar(range(SEQUENCE_SIZE), result)
+
+    plt.show()
+
+
+if __name__ == '__main__':
     trainning_seq_pairs, trainning_labels = load_trainning_date()
 
     model = create_model()
-    model.fit(trainning_seq_pairs, trainning_labels, epochs=10)
+    history = model.fit(trainning_seq_pairs,
+                        trainning_labels, epochs=TRAINNING_EPOCH)
+    show_history(history)
 
     test_seq_pairs, test_labels = load_test_date()
     test_loss, test_acc = model.evaluate(test_seq_pairs, test_labels)
-    print("\nTest accuracy: {}\n".format(test_acc))
+    print('\nTest accuracy: {}'.format(test_acc))
+    print('Test loss: {}\n'.format(test_loss))
 
     data = random_seq_pairs(1)
     result = model.predict(data)
+
     show_result(data[0], result[0])
