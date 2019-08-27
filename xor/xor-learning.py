1fc98726b7f5ef97f2358cc5d45de7d1a92122bb
Jianshao <jms_wu@hotmail.com>
2019-08-27 19:26:57 +0800

    optimize

--- xor/xor-learning.py
+++ xor/xor-learning.py
@@ -1,13 +1,14 @@
 import random
+
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
-import matplotlib.pyplot as plt
 
 SEQUENCE_SIZE = 10
 TRAINNING_DATA_SIZE = 5000
 TEST_DATA_SIZE = 100
-TRAINNING_EPOCH = 20
+TRAINNING_EPOCH = 10
 
 
 def seq_xor(sequence_pair):
@@ -56,26 +57,41 @@ def create_model():
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(2, SEQUENCE_SIZE)),
         keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(128, activation=tf.nn.relu),
         keras.layers.Dense(SEQUENCE_SIZE, activation=tf.nn.sigmoid)
     ])
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+                  loss=keras.losses.binary_crossentropy,
+                  metrics=[keras.metrics.binary_accuracy])
     return model
 
 
 def show_history(history):
     plt.figure(figsize=(9, 6))
 
-    accuracy = history.history['acc']
+    accuracy = history.history['binary_accuracy']
     loss = history.history['loss']
 
     plt.plot(accuracy, c='blue')
     for i in range(len(accuracy)):
-        plt.scatter(i, accuracy[i], color="blue")
+        plt.scatter(i, accuracy[i], color='blue')
+    last_index = len(accuracy) - 1
+    plt.annotate(accuracy[last_index],
+                 xy=(last_index, accuracy[last_index]),
+                 xytext=(-20, -50), textcoords='offset points',
+                 arrowprops=dict(facecolor='black', shrink=0.05, width=1),
+                 ha='center')
+
     plt.plot(loss, color='red')
     for i in range(len(loss)):
-        plt.scatter(i, loss[i], color="red")
+        plt.scatter(i, loss[i], color='red')
+    last_index = len(loss) - 1
+    plt.annotate(loss[last_index],
+                 xy=(last_index, loss[last_index]),
+                 xytext=(-20, 50), textcoords='offset points',
+                 arrowprops=dict(facecolor='black', shrink=0.05, width=1),
+                 ha='center')
+
     plt.title('trainning history')
     plt.xlabel('epoch')
     plt.xticks(range(TRAINNING_EPOCH), range(1, TRAINNING_EPOCH + 1))
@@ -107,18 +123,15 @@ def show_result(data, result):
 
 if __name__ == '__main__':
     trainning_seq_pairs, trainning_labels = load_trainning_date()
+    test_seq_pairs, test_labels = load_test_date()
 
     model = create_model()
     history = model.fit(trainning_seq_pairs,
-                        trainning_labels, epochs=TRAINNING_EPOCH)
+                        trainning_labels,
+                        validation_data=(test_seq_pairs, test_labels),
+                        epochs=TRAINNING_EPOCH)
     show_history(history)
-
-    test_seq_pairs, test_labels = load_test_date()
-    test_loss, test_acc = model.evaluate(test_seq_pairs, test_labels)
-    print('\nTest accuracy: {}'.format(test_acc))
-    print('Test loss: {}\n'.format(test_loss))
 
     data = random_seq_pairs(1)
     result = model.predict(data)
-
     show_result(data[0], result[0])
