1f00f548d78d39d27b2f6fbfe4b599854b620127
Jianshao <jms_wu@hotmail.com>
2019-09-03 14:54:43 +0800

    improve

--- xor/xor-learning.py
+++ xor/xor-learning.py
@@ -7,7 +7,7 @@ from tensorflow import keras
 
 SEQUENCE_SIZE = 10
 TRAINING_DATA_SIZE = 5000
-TEST_DATA_SIZE = 100
+TEST_DATA_SIZE = 500
 TRAINING_EPOCH = 10
 
 
@@ -71,33 +71,39 @@ def show_history(history):
     plt.figure(figsize=(9, 6))
 
     accuracy = history.history['binary_accuracy']
+    val_accuracy = history.history['val_binary_accuracy']
     loss = history.history['loss']
+    val_loss = history.history['val_loss']
 
-    plt.plot(accuracy, c='blue')
-    for i in range(len(accuracy)):
-        plt.scatter(i, accuracy[i], color='blue')
+    plt.plot(accuracy, '.b-')
+    plt.plot(val_accuracy, '.b--')
     last_index = len(accuracy) - 1
-    plt.annotate(accuracy[last_index],
+    plt.annotate('accuracy: %f' % accuracy[last_index],
                  xy=(last_index, accuracy[last_index]),
-                 xytext=(-20, -50), textcoords='offset points',
-                 arrowprops=dict(facecolor='black', shrink=0.05, width=1),
-                 ha='center')
+                 xytext=(-30, -50), textcoords='offset points', ha='center',
+                 arrowprops=dict(facecolor='black', arrowstyle='-|>'))
+    plt.annotate('val_accuracy: %f' % val_accuracy[last_index],
+                 xy=(last_index, val_accuracy[last_index]),
+                 xytext=(-90, -30), textcoords='offset points', ha='center',
+                 arrowprops=dict(facecolor='black', arrowstyle='-|>'))
 
-    plt.plot(loss, color='red')
-    for i in range(len(loss)):
-        plt.scatter(i, loss[i], color='red')
+    plt.plot(loss, '.r-')
+    plt.plot(val_loss, '.r--')
     last_index = len(loss) - 1
-    plt.annotate(loss[last_index],
+    plt.annotate('loss: %f' % loss[last_index],
                  xy=(last_index, loss[last_index]),
-                 xytext=(-20, 50), textcoords='offset points',
-                 arrowprops=dict(facecolor='black', shrink=0.05, width=1),
-                 ha='center')
+                 xytext=(-20, 50), textcoords='offset points', ha='center',
+                 arrowprops=dict(facecolor='black', arrowstyle='-|>'))
+    plt.annotate('val_loss: %f' % val_loss[last_index],
+                 xy=(last_index, val_loss[last_index]),
+                 xytext=(-80, 30), textcoords='offset points', ha='center',
+                 arrowprops=dict(facecolor='black', arrowstyle='-|>'))
 
     plt.title('Training Loss & Accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Loss/Accuracy')
     plt.xticks(range(TRAINING_EPOCH), range(1, TRAINING_EPOCH + 1))
-    plt.legend(['accuracy', 'loss'], loc='center right')
+    plt.legend(['accuracy', 'val_accuracy', 'loss', 'val_loss'], loc='center right')
 
     plt.show()
 
@@ -107,18 +113,21 @@ def show_example(data, example):
 
     plt.subplot(3, 1, 1)
     plt.xticks(range(SEQUENCE_SIZE), range(1, SEQUENCE_SIZE + 1))
+    plt.yticks([1])
     plt.ylabel('seq 1')
-    plt.bar(range(SEQUENCE_SIZE), data[0])
+    plt.bar(range(SEQUENCE_SIZE), data[0], width=1, edgecolor='black')
 
     plt.subplot(3, 1, 2)
     plt.xticks(range(SEQUENCE_SIZE), range(1, SEQUENCE_SIZE + 1))
+    plt.yticks([1])
     plt.ylabel('seq 2')
-    plt.bar(range(SEQUENCE_SIZE), data[1])
+    plt.bar(range(SEQUENCE_SIZE), data[1], width=1, edgecolor='black')
 
     plt.subplot(3, 1, 3)
     plt.xticks(range(SEQUENCE_SIZE), range(1, SEQUENCE_SIZE + 1))
+    plt.yticks([1])
     plt.ylabel('xor')
-    plt.bar(range(SEQUENCE_SIZE), example)
+    plt.bar(range(SEQUENCE_SIZE), example, width=1, edgecolor='black')
 
     plt.show()
 
