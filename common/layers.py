2b1eb4b100ea37431f257aa361de4b927be57737
Jianshao <jms_wu@hotmail.com>
2019-09-26 19:35:04 +0800

    update

--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,16 @@
+import tensorflow as tf
 
+class SimepleFlatten():
+    def __init__(self, units, input_shape=None):
+        self.units = units
+        self.input_shape = input_shape
+
+    def __call__(self, inputs):
+        return inputs
+
+    @property
+    def trainable_variables(self):
+        pass
 
 class SimepleDense():
     def __init__(self, units, activation=None):
@@ -7,8 +19,11 @@ class SimepleDense():
         self.kernel = None
         self.bias = None
 
-    def __call__(self, x):
-        y = x * self.kernel + self.bias
+    def __call__(self, inputs):
+        self.kernel = tf.Variable()
+        self.bias = tf.Variable()
+
+        y = inputs * self.kernel + self.bias
         if self.activation:
             y = self.activation(y)
         return y
