90f66b1d0f821b5ca6d7eb739cb1a1792c0284da
Jianshao <jms_wu@hotmail.com>
2019-09-27 16:13:38 +0800

    add layer use

---
+++ bitwise_xor/xor_tf2_layer.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from tensorflow import keras
+
+from common import data_utils as utils
+from common import models
+from common import vis_utils as vis
+from common import layers
+from common.data_utils import SEQUENCE_SIZE, TRAIN_EPOCH
+
+if __name__ == '__main__':
+    dataset = tf.data.Dataset.from_tensor_slices(utils.gen_xor_train_data())
+    dataset = dataset.batch(32)
+    test_seq_pairs, test_labels = utils.gen_xor_test_data()
+
+    model = models.SimpleModel([
+        layers.SimpleFlatten(input_shape=(2, SEQUENCE_SIZE)),
+        layers.SimpleDense(64, activation=keras.activations.relu),
+        layers.SimpleDense(64, activation=keras.activations.relu),
+        layers.SimpleDense(64, activation=keras.activations.relu),
+        layers.SimpleDense(SEQUENCE_SIZE, activation=keras.activations.sigmoid)])
+
+    model.compile(optimizer=keras.optimizers.Adam(),
+                  loss=keras.losses.BinaryCrossentropy(),
+                  metrics=[keras.metrics.BinaryAccuracy()])
+
+    callback = vis.VisualizationCallback(show_model=True, runtime_plot=True)
+    history = model.fit(dataset=dataset,
+                        epochs=TRAIN_EPOCH,
+                        validation_data=(test_seq_pairs, test_labels),
+                        callbacks=[callback])
+
+    example_data = utils.random_seq_pairs(1)
+    example_result = model.predict(example_data)
+    vis.build_multi_bar_figure(['seq1', 'seq2', 'xor'],
+                               [example_data[0][0], example_data[0][1], example_result[0]])
+    vis.show_all()
--- common/layers.py
+++ common/layers.py
@@ -1,33 +1,65 @@
+import numpy as np
 import tensorflow as tf
+from tensorflow import keras
 
-class SimepleFlatten():
-    def __init__(self, units, input_shape=None):
+
+class SimpleFlatten():
+    def __init__(self,
+                 units=None,
+                 input_shape=None):
         self.units = units
         self.input_shape = input_shape
 
+    @property
+    def trainable_variables(self):
+        return {}
+
     def __call__(self, inputs):
-        return inputs
+        input_shape = inputs.shape
+        return tf.reshape(inputs, (-1, np.prod(input_shape[1:], dtype=int)))
+
+
+class SimpleDense():
+    def __init__(self,
+                 units,
+                 activation=None,
+                 kernel_initializer=keras.initializers.GlorotUniform(),
+                 bias_initializer=keras.initializers.Zeros()):
+        self.units = units
+        self.activation = activation
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.weights = []
+        self.kernel = None
+        self.bias = None
+        self._built = False
 
     @property
     def trainable_variables(self):
-        pass
-
-class SimepleDense():
-    def __init__(self, units, activation=None):
-        self.units = units
-        self.activation = activation
-        self.kernel = None
-        self.bias = None
+        return self.weights
 
     def __call__(self, inputs):
-        self.kernel = tf.Variable()
-        self.bias = tf.Variable()
+        if not inputs.dtype.is_floating:
+            inputs = tf.dtypes.cast(inputs, dtype=tf.float32)
 
-        y = inputs * self.kernel + self.bias
+        self._maybe_build(inputs.shape)
+
+        y = tf.matmul(inputs, self.kernel)
+        y = tf.nn.bias_add(y, self.bias)
+
         if self.activation:
             y = self.activation(y)
         return y
 
-    @property
-    def trainable_variables(self):
-        pass
+    def _maybe_build(self, input_shape):
+        if not self._built:
+            self.kernel = self.add_weight(
+                'kernel', (input_shape[-1], self.units), self.kernel_initializer)
+            self.bias = self.add_weight(
+                'bias', (self.units), self.bias_initializer)
+            self._built = True
+
+    def add_weight(self, name, shape=None, initializer=None):
+        weight = tf.Variable(initializer(shape), name=name, shape=shape)
+        self.weights.append(weight)
+        return weight
