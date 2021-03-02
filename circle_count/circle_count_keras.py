6b3348a53e6089f6f2e219153c30b13b9721349c
Jianshao <787903+jianshaow@users.noreply.github.com>
2021-03-02 18:41:11 +0800

    update

--- circle_count/circle_count_keras.py
+++ circle_count/circle_count_keras.py
@@ -6,24 +6,23 @@ from common.img_utils import TRAIN_EPOCH, CIRCLES_MAX
 
 
 def run():
-    (train_data, _, train_labels), (test_data, _, test_labels) = utils.load_data()
+    (train_data, train_label), (test_data, test_label) = utils.load_reg_data()
     train_data, test_data = train_data/255.0, test_data/255.0
 
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(100, 100)),
-        keras.layers.Dense(64, activation='relu'),
-        keras.layers.Dense(64, activation='relu'),
-        keras.layers.Dense(CIRCLES_MAX, 'sigmoid')
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(CIRCLES_MAX, activation='softmax')
     ])
 
     model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
-                  metrics=['categorical_accuracy'])
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
 
     callback = vis.VisualizationCallback(
         show_model=True, show_metrics=True, dynamic_plot=True)
-    model.fit(train_data, train_labels,
-              validation_data=(test_data, test_labels),
+    model.fit(train_data, train_label,
+              validation_data=(test_data, test_label),
               epochs=TRAIN_EPOCH,
               callbacks=[callback])
 
--- common/img_utils.py
+++ common/img_utils.py
@@ -8,15 +8,16 @@ import random
 TRAIN_DATA_SIZE = 5000
 TEST_DATA_SIZE = 500
 TRAIN_EPOCH = 10
-CIRCLES_MAX = 5
+CIRCLES_MAX = 6
 
-CIRCLE_COUNT_DATA_FILE = os.path.join(os.path.expanduser('~'), '.dataset/circle_count')
+CIRCLE_COUNT_DATA_FILE = os.path.join(
+    os.path.expanduser('~'), '.dataset/circle_count')
 
 
 def random_circles_imgs(handle, size=1):
     fig = plt.figure(figsize=(1, 1))
     for i in range(size):
-        circles = random.randint(1, CIRCLES_MAX)
+        circles = random.randint(0, CIRCLES_MAX - 1)
 
         x = []
         y = []
@@ -62,7 +63,7 @@ def gen_circle_count_data(size=1):
     def handle(i, data, label):
         datas[i] = data
         reg_labels[i] = label
-        cls_labels[i][label - 1] = 1
+        cls_labels[i][label] = 1
     random_circles_imgs(handle, size)
     return datas, reg_labels, cls_labels
 
@@ -86,18 +87,41 @@ def load_data():
         test_cls_label = f['test_cls_label']
         return (train_data, train_reg_label, train_cls_label), (test_data, test_reg_label, test_cls_label)
 
-def __test():
-    (train_data, train_reg_labels, train_cls_label), (test_data, test_reg_label, test_cls_label) = load_data()
-    print(train_data.shape, train_data.dtype)
-    print(train_reg_labels[0], train_cls_label[0])
-    plt.imshow(train_data[0])
+
+def load_cls_data():
+    (train_data, _, train_label), (test_data, _, test_label) = load_data()
+    return (train_data, train_label), (test_data, test_label)
+
+
+def load_reg_data():
+    (train_data, train_label, _), (test_data, test_label, _) = load_data()
+    return (train_data, train_label), (test_data, test_label)
+
+
+def show_data(x_train, y_train, class_mapping=None):
+    plt.figure(figsize=(10, 10))
+    for i in range(25):
+        plt.subplot(5, 5, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(x_train[i], cmap=plt.cm.binary)
+        xlabel = y_train[i] if class_mapping == None else class_mapping[y_train[i]]
+        plt.xlabel(xlabel)
     plt.show()
+
+
+def __test():
+    (train_data, train_reg_label, train_cls_label), (test_data,
+                                                     test_reg_label, test_cls_label) = load_data()
+    print(train_data.shape, train_data.dtype)
+    print(train_reg_label[0], train_cls_label[0])
     print(test_data.shape, test_data.dtype)
     print(test_reg_label[0], test_cls_label[0])
-    plt.imshow(test_data[0])
-    plt.show()
+
+    show_data(train_data, train_reg_label)
 
 
 if __name__ == '__main__':
-    save_circle_count_dataset()
-    # __test()
+    # save_circle_count_dataset()
+    __test()
