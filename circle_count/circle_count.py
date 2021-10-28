d11eb1e33b45c0fb0fbcba694b73d37a8989cd4f
Jianshao <787903+jianshaow@users.noreply.github.com>
2021-10-29 01:07:45 +0800

    extract class and function

--- circle_count/circle_count.py
+++ circle_count/circle_count.py
@@ -1,200 +1,59 @@
-import os
-import shutil
-
 from common import img_utils as img
-from common import vis_utils as vis
-from common.img_utils import CIRCLES_MAX, TRAIN_EPOCH
-from tensorflow import keras
+from common.img_utils import TRAIN_EPOCH
 
-MODEL_NAME_PREFIX = 'circle_count'
-MODEL_BASE_DIR = os.path.join(os.path.expanduser('~'), '.model')
-
-MODEL_PARAMS = (2, 64)
-LEARNING_RATE = 0.0001
-
-ERROR_BATCH_SIZE = 100
-ERROR_DATA_SIZE = 1000
+import models
+import utils
+from models import LEARNING_RATE, MODEL_PARAMS
 
 DATA_CONFIG = img.data_config(6)
 
 
-def get_model_path(name):
-    return os.path.join(MODEL_BASE_DIR, name), os.path.join(MODEL_BASE_DIR, name + '.old')
-
-
-def get_model_name(hidden_layers, units):
-    return MODEL_NAME_PREFIX + '.' + str(hidden_layers) + '-' + str(units)
-
-
-def prepare_data():
-    (x_train, y_train), (x_test, y_test) = img.load_cls_data(DATA_CONFIG)
-    x_train, x_test = x_train/255.0, x_test/255.0
-    return x_train, y_train, (x_test, y_test)
-
-
-def prepare_error_data():
-    x_train, y_train = img.load_cls_error_data(DATA_CONFIG)
-    _, (x_test, y_test) = img.load_cls_data(DATA_CONFIG)
-    x_train = x_train/255.0
-    return x_train, y_train, (x_test[:100], y_test[:100])
-
-
-def build_model(model_params=MODEL_PARAMS, learning_rate=LEARNING_RATE):
-    hidden_layers, units = model_params
-
-    model_name = get_model_name(hidden_layers, units)
-    model = keras.Sequential([keras.layers.Flatten(
-        input_shape=(100, 100))], name=model_name)
-    for _ in range(hidden_layers):
-        model.add(keras.layers.Dense(units, activation='relu'))
-    model.add(keras.layers.Dense(CIRCLES_MAX, activation='softmax'))
-
-    return model
-
-
-def compile_model(model, learning_rate=LEARNING_RATE):
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
-
-def train_model(model, x_train, y_train, epochs=TRAIN_EPOCH, validation_data=None):
-    callback = vis.VisualizationCallback(
-        show_model=True, show_metrics=True, dynamic_plot=True)
-
-    model.fit(x_train, y_train, epochs=epochs,
-              validation_data=validation_data,
-              callbacks=[callback])
-
-
-def verify_model(model, data=img.gen_circles_data(DATA_CONFIG, size=20)):
-    images, nums, num_labels = data
-
-    predictions = model.predict(images)
-    img.show_images(images, nums, predictions, title='predict result')
-
-    evaluation = model.evaluate(images, num_labels)
-    print('evaluation: ', evaluation)
-
-
-def save_model(model):
-    model_path, model_old_path = get_model_path(model.name)
-    if os.path.exists(model_path):
-        if os.path.exists(model_old_path):
-            shutil.rmtree(model_old_path)
-        os.rename(model_path, model_old_path)
-    model.save(model_path)
-    print('model[' + model.name + '] saved')
-
-
-def load_model(model_params=MODEL_PARAMS):
-    hidden_layers, units = model_params
-    model_path, _ = get_model_path(get_model_name(hidden_layers, units))
-    return keras.models.load_model(model_path)
-
-
-def first_run(model_params=MODEL_PARAMS, data=prepare_data(), learning_rate=LEARNING_RATE, dry_run=False):
+def first_run(model_params=MODEL_PARAMS, data=utils.prepare_data(), learning_rate=LEARNING_RATE, dry_run=False):
     x_train, y_train, validation_data = data
 
-    model = build_model(model_params, learning_rate)
-
-    compile_model(model, learning_rate)
-
-    train_model(model, x_train, y_train, validation_data=validation_data)
-
-    verify_model(model)
+    model = models.Model(model_params)
+    model.build()
+    model.compile(learning_rate)
+    model.train(x_train, y_train, validation_data=validation_data)
+    model.verify(img.gen_circles_data(DATA_CONFIG, size=100))
 
     if not dry_run:
-        save_model(model)
+        model.save()
 
     return model
 
 
-def re_run(model_params=MODEL_PARAMS, data=prepare_data(), learning_rate=None, epochs=TRAIN_EPOCH):
+def re_run(model_params=MODEL_PARAMS, data=utils.prepare_data(), learning_rate=None, epochs=TRAIN_EPOCH):
     x_train, y_train, validation_data = data
 
-    model = load_model(model_params)
+    model = models.Model(model_params)
+    model.load()
 
     if learning_rate is not None:
-        compile_model(model, learning_rate)
+        model.compile(learning_rate)
 
-    train_model(model, x_train, y_train, epochs=epochs,
+    model.train(x_train, y_train, epochs=epochs,
                 validation_data=validation_data)
 
-    verify_model(model)
+    model.verify(img.gen_circles_data(DATA_CONFIG, size=100))
 
-    save = input('save model[' + model.name + ']? (y|n): ')
-    if save == 'y':
-        save_model(model)
+    model.save(ask=True)
 
     return model
 
 
-def build_error_data(model_params=MODEL_PARAMS, append=False, dry_run=False):
-    model = load_model(model_params)
-
-    if not dry_run:
-        x, y_reg, y_cls = img.zero_data(ERROR_DATA_SIZE)
-
-    added = 0
-    handled = 0
-    while(added < ERROR_DATA_SIZE):
-        images, circle_nums, _ = img.gen_circles_data(
-            DATA_CONFIG, ERROR_BATCH_SIZE)
-        predictions = model.predict(images)
-        for i in range(ERROR_BATCH_SIZE):
-            if predictions[i][circle_nums[i]] == 0:
-                if dry_run:
-                    print(predictions[i], circle_nums[i])
-                else:
-                    x[added] = images[i]
-                    y_reg[added] = circle_nums[i]
-                    y_cls[added][circle_nums[i]] = 1
-                added += 1
-                if (added + 5) % 10 == 0:
-                    if dry_run:
-                        print(img.num_to_cls(0), 0)
-                    else:
-                        x[added] = img.blank_image()
-                        y_reg[added] = 0
-                        y_cls[added][0] = 1
-                    added += 1
-                if added >= ERROR_DATA_SIZE:
-                    break
-        handled += ERROR_BATCH_SIZE
-        print(added, 'error data added per', handled)
-
-    if not dry_run:
-        img.save_error_dataset((x, y_reg, y_cls), DATA_CONFIG, append)
-
-
-def demo_model(model_params=MODEL_PARAMS, data=img.gen_circles_data(DATA_CONFIG, size=20)):
-    model = load_model(model_params)
-    verify_model(model, data)
-
-
-def load_sample_data(size=20):
-    (x_train, y_reg_train, y_cls_train), _ = img.load_data(DATA_CONFIG)
-    return x_train[:size], y_reg_train[:size], y_cls_train[:size]
-
-
-def load_sample_error_data(size=20):
-    x_train, y_reg_train, y_cls_train = img.load_error_data(DATA_CONFIG)
-    return x_train[:size], y_reg_train[:size], y_cls_train[:size]
+def demo_model(model_params=MODEL_PARAMS, data=img.gen_circles_data(DATA_CONFIG, size=100)):
+    model = models.Model(model_params)
+    model.load()
+    model.verify(data)
 
 
 if __name__ == '__main__':
     # first_run()
     # first_run(learning_rate=0.00001)
-    # first_run(dry_run=False)
-    # re_run()
-    # re_run(learning_rate=LEARNING_RATE)
-    # re_run(learning_rate=0.00001)
-    # re_run(data=prepare_error_data(), learning_rate=0.000001, epochs=50)
+    # first_run(dry_run=True)
+    re_run()
+    # re_run(data=prepare_error_data(), learning_rate=0.000001, epochs=10)
     # demo_model()
-    demo_model(data=img.gen_circles_data(img.data_config(6), size=20))
     # demo_model(data=load_sample_data(1000))
     # demo_model(data=load_sample_error_data(1000))
-    # build_error_data()
-    # build_error_data(append=True)
-    # build_error_data(dry_run=True)
---
+++ circle_count/models.py
@@ -0,0 +1,100 @@
+import os
+import shutil
+
+from common import img_utils as img
+from common import vis_utils as vis
+from common.img_utils import CIRCLES_MAX, TRAIN_EPOCH
+from tensorflow import keras
+
+MODEL_NAME_PREFIX = 'circle_count'
+MODEL_BASE_DIR = os.path.join(os.path.expanduser('~'), '.model')
+
+MODEL_PARAMS = (2, 64)
+LEARNING_RATE = 0.00001
+
+
+class Model():
+
+    def __init__(self, params):
+        self.__params = params
+        self.__compiled = False
+        self.model = None
+
+    def build(self):
+        if self.model is not None:
+            raise Exception('model is initialized')
+
+        hidden_layers, units = self.__params
+
+        model_name = _get_model_name(hidden_layers, units)
+        self.model = keras.Sequential(
+            [keras.layers.Flatten(input_shape=(100, 100))], name=model_name)
+        for _ in range(hidden_layers):
+            self.model.add(keras.layers.Dense(units, activation='relu'))
+        self.model.add(keras.layers.Dense(CIRCLES_MAX, activation='softmax'))
+
+    def load(self):
+        if self.model is not None:
+            raise Exception('model is initialized')
+
+        hidden_layers, units = self.__params
+        model_path, _ = _get_model_path(
+            _get_model_name(hidden_layers, units))
+        self.model = keras.models.load_model(model_path)
+        self.__compiled = True
+
+    def compile(self, learning_rate=LEARNING_RATE):
+        if self.model is None:
+            raise Exception(
+                'model is not initialized, call build or load method')
+
+        self.model.compile(optimizer=keras.optimizers.Adam(learning_rate),
+                           loss='categorical_crossentropy',
+                           metrics=['accuracy'])
+        self.__compiled = True
+
+    def train(self, x_train, y_train, epochs=TRAIN_EPOCH, validation_data=None):
+        if not self.__compiled:
+            raise Exception('model is not compiled yet, call compile')
+
+        callback = vis.VisualizationCallback(
+            show_model=True, show_metrics=True, dynamic_plot=True)
+
+        self.model.fit(x_train, y_train, epochs=epochs,
+                       validation_data=validation_data,
+                       callbacks=[callback])
+
+    def predict(self, x):
+        return self.model.predict(x)
+
+    def verify(self, data):
+        x, y_reg, y_cls = data
+
+        predictions = self.model.predict(x)
+        img.show_images(x, y_reg, predictions, title='predict result')
+
+        evaluation = self.model.evaluate(x, y_cls)
+        print('evaluation: ', evaluation)
+
+    def save(self, ask=False):
+        if ask:
+            save = input('save model[' + self.model.name + ']? (y|n): ')
+            if save != 'y':
+                print('model[' + self.model.name + '] not saved')
+                return
+
+        model_path, model_old_path = _get_model_path(self.model.name)
+        if os.path.exists(model_path):
+            if os.path.exists(model_old_path):
+                shutil.rmtree(model_old_path)
+            os.rename(model_path, model_old_path)
+        self.model.save(model_path)
+        print('model[' + self.model.name + '] saved')
+
+
+def _get_model_path(name):
+    return os.path.join(MODEL_BASE_DIR, name), os.path.join(MODEL_BASE_DIR, name + '.old')
+
+
+def _get_model_name(hidden_layers, units):
+    return MODEL_NAME_PREFIX + '.' + str(hidden_layers) + '-' + str(units)
