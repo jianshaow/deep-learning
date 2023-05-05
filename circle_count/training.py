2fd2431d3f43a1e889d194d42cc5b980c9538a7e
Jianshao <787903+jianshaow@users.noreply.github.com>
2023-05-05 12:33:14 +0800

    update

--- circle_count/training.py
+++ circle_count/training.py
@@ -33,7 +33,7 @@ def re_run(data=utils.load_data(), learning_rate=LEARNING_RATE, epochs=RERUN_EPO
 def demo_model(data=utils.gen_sample_data(size=100)):
     model = __new_model()
     model.load()
-    model.compile(LEARNING_RATE)
+    model.compile()
     model.verify(data)
 
 
@@ -49,10 +49,9 @@ if __name__ == '__main__':
     re_run()
     # re_run(learning_rate=0.0001)
     # re_run(learning_rate=0.000001)
-    # re_run(data=utils.prepare_error_data())
-    # re_run(data=utils.prepare_error_data(), learning_rate=0.0001)
-    # re_run(data=utils.prepare_error_data(), learning_rate=0.000001)
+    # re_run(data=utils.load_error_data())
+    # re_run(data=utils.load_error_data(), learning_rate=0.0001)
+    # re_run(data=utils.load_error_data(), learning_rate=0.000001)
     # demo_model()
-    # demo_model(data=utils.gen_sample_data(size=100))
     # demo_model(data=utils.load_sample_data(size=1000))
     # demo_model(data=utils.load_sample_error_data(size=1000))
